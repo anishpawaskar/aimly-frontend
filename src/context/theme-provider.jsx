f1d9df6c5e705201689ae650f11ad4c31d32146a
anishpawaskar <anishpawaskar@gmail.com>
2025-06-10 13:44:03 +0530

    feat: create theme provider

---
+++ src/context/theme-provider.jsx
@@ -0,0 +1,25 @@
+import { createContext, useContext, useEffect, useState } from "react";
+
+const ThemeContext = createContext();
+
+export const ThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem("theme") || "light";
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const toggleTheme = (value) => {
+    setTheme(value);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+
+export const useTheme = () => useContext(ThemeContext);
