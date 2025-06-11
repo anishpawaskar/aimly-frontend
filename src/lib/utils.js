dfe6a665f8b636b5572058ce42b5be6cd8be0cc6
anishpawaskar <anishpawaskar@gmail.com>
2025-06-11 19:27:40 +0530

    feat: create utility functions for calculating x and y position

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,6 +1,25 @@
+import { GAP_BETWEEN_SCREEN } from "@/constants";
 import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export const cn = (...inputs) => {
   return twMerge(clsx(inputs));
+};
+
+export const clampX = (x, contentWidth) => {
+  const windowWidth = window.innerWidth;
+
+  return Math.min(
+    Math.max(x, GAP_BETWEEN_SCREEN),
+    windowWidth - contentWidth - GAP_BETWEEN_SCREEN
+  );
+};
+
+export const clampY = (y, contentHeight) => {
+  const windowHeight = window.innerHeight;
+
+  return Math.min(
+    Math.max(y, GAP_BETWEEN_SCREEN),
+    windowHeight - contentHeight - GAP_BETWEEN_SCREEN
+  );
 };
