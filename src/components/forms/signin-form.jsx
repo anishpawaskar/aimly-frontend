c4b5cc963d8856040b1fc658da03f84f90d11544
anishpawaskar <anishpawaskar@gmail.com>
2025-06-12 15:43:18 +0530

    refactor: signin page

---
+++ src/components/forms/signin-form.jsx
@@ -0,0 +1,78 @@
+import { isValidEmail, validateInputs } from "@/lib/utils";
+import { useState } from "react";
+import { AuthForm } from "./auth-form";
+
+export const SigninForm = () => {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+  const [validationErrors, setValidationErrors] = useState({
+    email: "",
+    password: "",
+    serverError: "",
+  });
+
+  const onChange = (e) => {
+    const { name, value } = e.target;
+    setFormData((prevFormData) => ({ ...prevFormData, [name]: value }));
+
+    setValidationErrors((prevValidationErrors) => ({
+      ...prevValidationErrors,
+      [name]: value
+        ? ""
+        : `${name === "email" ? "Email address" : "Password"} can't be empty.`,
+      serverError: "",
+    }));
+  };
+
+  const validateForm = () => {
+    const valuesToValidate = {
+      email: formData.email,
+      password: formData.password,
+    };
+
+    const defaultErrors = {
+      email: "Email address can't be empty.",
+      password: "Password can't be empty.",
+    };
+
+    let errors = validateInputs(valuesToValidate, defaultErrors);
+
+    if (!isValidEmail(formData.email) && formData.email) {
+      errors.email = "Invalid email format.";
+    }
+
+    if (formData.password.length < 6) {
+      errors.password = "Password requires at least 6 characters.";
+    }
+
+    return errors;
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    const errors = validateForm();
+
+    if (Object.values(errors).some(Boolean)) {
+      setValidationErrors((prevValidationErrors) => ({
+        ...prevValidationErrors,
+        ...errors,
+      }));
+      return;
+    }
+
+    console.log("logged in user");
+  };
+
+  return (
+    <AuthForm
+      heading={"Sign In"}
+      authType={"signIn"}
+      onChange={onChange}
+      onSubmit={onSubmit}
+      formData={formData}
+      validationErrors={validationErrors}
+    />
+  );
+};
