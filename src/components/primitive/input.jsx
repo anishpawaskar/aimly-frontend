0e5b9e616e909cd410b6ce6c66beb4dbaaea22a9
anishpawaskar <anishpawaskar@gmail.com>
2025-06-13 21:18:07 +0530

    refactor: add forward ref for passing input ref

--- src/components/primitive/input.jsx
+++ src/components/primitive/input.jsx
@@ -1,11 +1,12 @@
 import { cn } from "@/lib/utils";
 import { cva } from "class-variance-authority";
+import { forwardRef } from "react";
 
 const inputVariants = cva(
   "h-10 w-full p-2 text-sm border border-gray/10 rounded-md bg-transparent focus:border-[var(--primary-color)] outline-none"
 );
 
-export const Input = ({ className, type, ref, ...props }) => {
+export const Input = forwardRef(({ className, type, ...props }, ref) => {
   return (
     <input
       type={type}
@@ -14,4 +15,6 @@ export const Input = ({ className, type, ref, ...props }) => {
       {...props}
     />
   );
-};
+});
+
+Input.displayName = "Input";
