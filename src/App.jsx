8a54004b2ff3ca2aceb95111c199d3bb7e5b1291
anishpawaskar <anishpawaskar@gmail.com>
2025-06-14 13:48:44 +0530

    fix: add task id route

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router";
+import { NavLink, Route, Routes } from "react-router";
 import "./App.css";
 import Signin from "./pages/auth/signin";
 import AuthLayout from "./pages/auth/layout";
@@ -10,20 +10,39 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/" element={<h1 className="text-2xl">Hello</h1>} />
+        <Route
+          path="/"
+          element={
+            <div>
+              <h1 className="text-2xl">Hello</h1>
+              <NavLink
+                to={"/"}
+                className={({ isActive }) =>
+                  isActive ? "text-red-300" : "text-green-300"
+                }
+              >
+                {({ isActive }) => {
+                  console.log("isActive", isActive);
+                  return <p>{isActive ? "active" : "pending"}</p>;
+                }}
+              </NavLink>
+            </div>
+          }
+        />
         <Route element={<AuthLayout />}>
           <Route path="signin" element={<Signin />} />
           <Route path="signup" element={<Signup />} />
         </Route>
-        <Route element={<TasksLayout />}>
+        <Route path="/tasks" element={<TasksLayout />}>
           <Route
-            path="tasks"
+            index
             element={
               <div>
                 <h1>Tasks</h1>
               </div>
             }
           />
+          <Route path=":taskId" element={<div>TaskId</div>} />
         </Route>
       </Routes>
     </>
