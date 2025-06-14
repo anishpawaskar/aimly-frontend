317af1a3bbd1b5ca6e2a3810135c0cb94fbb98da
anishpawaskar <anishpawaskar@gmail.com>
2025-06-14 13:45:03 +0530

    fix: accordion trigger height

--- src/components/primitive/accordion.jsx
+++ src/components/primitive/accordion.jsx
@@ -95,7 +95,7 @@ const AccordionTrigger = ({ children, asChild = false, className }) => {
   };
 
   return (
-    <div className="relative w-full">
+    <div className="relative w-full h-full">
       <ChevronDown
         size={16}
         className={cn(
