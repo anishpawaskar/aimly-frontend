6e43a3e9c6ce9e4bb9993ede6bf6486be78b0b30
anishpawaskar <anishpawaskar@gmail.com>
2025-06-17 13:15:45 +0530

    feat: create constant and utility functions for date picker

--- src/constants.js
+++ src/constants.js
@@ -3,3 +3,31 @@ export const GAP_BETWEEN_SCREEN = 16; //  1rem
 // Sort Order
 export const BASE_INTERVAL = 1099511627776;
 export const NETURAL_INTERVAL = 0;
+
+// Date Picker
+export const THIS_YEAR = new Date().getFullYear();
+export const THIS_MONTH = new Date().getMonth() + 1;
+export const WEEK_DAYS = {
+  Sunday: "Sun",
+  Monday: "Mon",
+  Tuesday: "Tue",
+  Wednesday: "Wed",
+  Thursday: "Thu",
+  Friday: "Fri",
+  Saturday: "Sat",
+};
+export const CALENDAR_MONTHS = {
+  January: "Jan",
+  February: "Feb",
+  March: "Mar",
+  April: "Apr",
+  May: "May",
+  June: "Jun",
+  July: "Jul",
+  August: "Aug",
+  September: "Sep",
+  October: "Oct",
+  November: "Nov",
+  December: "Dec",
+};
+export const CALENDAR_WEEKS = 6;
---
+++ src/lib/date-picker.helper.js
@@ -0,0 +1,106 @@
+import { CALENDAR_WEEKS, THIS_MONTH, THIS_YEAR } from "@/constants";
+
+export const getMonthDays = (month = THIS_MONTH, year = THIS_YEAR) => {
+  const month30Days = [4, 6, 9, 11];
+  const isLeapYear = year % 4 === 0;
+
+  return month === 2
+    ? isLeapYear
+      ? 29
+      : 28
+    : month30Days.includes(month)
+    ? 30
+    : 31;
+};
+
+export const getMonthFirstDay = (month = THIS_MONTH, year = THIS_YEAR) => {
+  return new Date(`${year}-${month}-1`).getDay() + 1;
+};
+
+export const isValidDate = (date) => {
+  const isDate = Object.prototype.toString.call(date) === "[object Date]";
+  const isValidDate = date && !isNaN(date.valueOf());
+
+  return isDate && isValidDate;
+};
+
+export const isSameMonth = (date, baseDate = new Date()) => {
+  if (!isValidDate(date) && isValidDate(baseDate)) {
+    return false;
+  }
+
+  const baseDateMonth = baseDate.getMonth() + 1;
+  const baseDateYear = baseDate.getFullYear();
+  const dateMonth = date.getMonth() + 1;
+  const dateYear = date.getFullYear();
+
+  return baseDateMonth === dateMonth && baseDateYear === dateYear;
+};
+
+export const isSameDay = (date, baseDate = new Date()) => {
+  if (!isValidDate(date) && isValidDate(baseDate)) {
+    return false;
+  }
+
+  const baseDateDay = baseDate.getDay() + 1;
+  const baseDateMonth = baseDate.getMonth() + 1;
+  const baseDateYear = baseDate.getFullYear();
+  const dateDay = date.getDay() + 1;
+  const dateMonth = date.getMonth() + 1;
+  const dateYear = date.getFullYear();
+
+  return (
+    baseDateDay === dateDay &&
+    baseDateMonth === dateMonth &&
+    baseDateYear === dateYear
+  );
+};
+
+export const getPrevMonth = (month, year) => {
+  const prevMonth = month > 1 ? month - 1 : 12;
+  const prevMonthYear = month > 1 ? year : year - 1;
+  return {
+    month: prevMonth,
+    year: prevMonthYear,
+  };
+};
+
+export const getNextMonth = (month, year) => {
+  const nextMonth = month < 12 ? month + 1 : 1;
+  const nextMonthYear = month < 12 ? year : year + 1;
+  return {
+    month: nextMonth,
+    year: nextMonthYear,
+  };
+};
+
+export const generateCalendar = (month = THIS_MONTH, year = THIS_YEAR) => {
+  const monthsDays = getMonthDays(month, year);
+  const monthFirstDay = getMonthFirstDay(month, year);
+
+  const daysFromPrevMonth = monthFirstDay - 1;
+  const daysFromNextMonth =
+    CALENDAR_WEEKS * 7 - (daysFromPrevMonth + monthsDays);
+
+  const { month: prevMonth, year: prevMonthYear } = getPrevMonth(month, year);
+  const { month: nextMonth, year: nextMonthYear } = getNextMonth(month, year);
+
+  const prevMonthDays = getMonthDays(prevMonth, prevMonthYear);
+
+  const prevMonthDates = [...new Array(daysFromPrevMonth)].map((_, idx) => {
+    const day = idx + 1 + (prevMonthDays - daysFromPrevMonth);
+    return [prevMonthYear, prevMonth, day];
+  });
+
+  const thisMonthDates = [...new Array(monthsDays)].map((_, idx) => {
+    const day = idx + 1;
+    return [year, month, day];
+  });
+
+  const nextMonthDates = [...new Array(daysFromNextMonth)].map((_, idx) => {
+    const day = idx + 1;
+    return [nextMonthYear, nextMonth, day];
+  });
+
+  return [...prevMonthDates, ...thisMonthDates, ...nextMonthDates];
+};
