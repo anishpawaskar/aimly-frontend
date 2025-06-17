1c7e32503dde651b23dc84281d17a629925a59e2
anishpawaskar <anishpawaskar@gmail.com>
2025-06-17 20:14:21 +0530

    feat: add month view and now we can toggle between month and date view

--- src/components/common/date-picker.jsx
+++ src/components/common/date-picker.jsx
@@ -58,6 +58,7 @@ const DATE_OPTIONS = [
 
 const DatePicker = ({ selectedDate, setSelectedDate }) => {
   const [isOpen, setIsOpen] = useState(false);
+  const [calendarView, setCalendarView] = useState("date");
 
   const monthsKeys = Object.keys(CALENDAR_MONTHS);
 
@@ -68,6 +69,7 @@ const DatePicker = ({ selectedDate, setSelectedDate }) => {
 
   const days = generateCalendar(selectedDate?.month, selectedDate?.year);
   const totalDaysInWeek = Object.keys(WEEK_DAYS).length;
+  const totalMonthsInYear = Object.keys(CALENDAR_MONTHS).length;
 
   const handlePrevMonth = () => {
     const { month, year } = getPrevMonth(
@@ -75,10 +77,20 @@ const DatePicker = ({ selectedDate, setSelectedDate }) => {
       selectedDate?.year ?? THIS_YEAR
     );
 
-    setSelectedDate((prevState) => ({ ...prevState, month, year }));
+    const payload = {};
+
+    if (calendarView === "date") {
+      payload.month = month;
+      payload.year = year;
+    } else {
+      payload.year = year - 1;
+    }
+
+    setSelectedDate((prevState) => ({ ...prevState, ...payload }));
   };
 
   const handleCurrentMonth = () => {
+    if (calendarView !== "date") return;
     setSelectedDate((prevState) => ({
       ...prevState,
       month: THIS_MONTH,
@@ -92,7 +104,20 @@ const DatePicker = ({ selectedDate, setSelectedDate }) => {
       selectedDate?.year ?? THIS_YEAR
     );
 
-    setSelectedDate((prevState) => ({ ...prevState, month, year }));
+    const payload = {};
+
+    if (calendarView === "date") {
+      payload.month = month;
+      payload.year = year;
+    } else {
+      payload.year = year + 1;
+    }
+
+    setSelectedDate((prevState) => ({ ...prevState, ...payload }));
+  };
+
+  const toggleCalendarView = () => {
+    setCalendarView((prevState) => (prevState === "date" ? "month" : "date"));
   };
 
   return (
@@ -192,10 +217,15 @@ const DatePicker = ({ selectedDate, setSelectedDate }) => {
           </ul>
           <div className="calendar-wrapper">
             <div className="calendar-header flex items-center justify-between gap-2.5">
-              <p className="text-sm w-[70%] text-gray/80">
-                <span className="current-month mr-[2px]">{currentMonth}</span>
+              <button
+                className={"text-sm w-[70%] text-gray/80 text-start pl-1.5"}
+                onClick={toggleCalendarView}
+              >
+                {calendarView === "date" && (
+                  <span className="current-month mr-[2px]">{currentMonth}</span>
+                )}
                 <span className="current-year">{currentYear}</span>
-              </p>
+              </button>
               <div className="w-[30%] flex items-center gap-0.5">
                 <IconButton
                   variant={"ghost"}
@@ -220,88 +250,139 @@ const DatePicker = ({ selectedDate, setSelectedDate }) => {
               </div>
             </div>
             <div className="calendar-body">
-              <ul className="flex flex-col">
-                <li className="calendar-weeks flex items-center justify-between text-xs">
-                  {Object.keys(WEEK_DAYS).map((day) => {
+              {calendarView === "month" && (
+                <ul className="flex flex-col">
+                  {[...new Array(totalMonthsInYear / 4)].map((_, idx) => {
                     return (
-                      <span
-                        key={day}
-                        className="h-7 w-7 flex items-center justify-center text-gray/40 text-center"
+                      <li
+                        key={idx}
+                        className="flex items-center justify-between"
                       >
-                        {WEEK_DAYS[day][0]}
-                      </span>
+                        {Object.keys(CALENDAR_MONTHS)
+                          .slice(
+                            idx * (totalMonthsInYear / 3),
+                            (idx + 1) * (totalMonthsInYear / 3)
+                          )
+                          .map((month) => {
+                            const monthIdx =
+                              Object.keys(CALENDAR_MONTHS).indexOf(month) + 1;
+
+                            const isSelected = selectedDate?.month
+                              ? selectedDate?.month === monthIdx
+                              : THIS_MONTH === monthIdx;
+
+                            const handleMonth = () => {
+                              setSelectedDate((prevState) => ({
+                                ...prevState,
+                                month: monthIdx,
+                              }));
+                              setCalendarView("date");
+                            };
+
+                            return (
+                              <button
+                                key={month}
+                                onClick={handleMonth}
+                                className={cn(
+                                  "h-8 w-8 rounded-full flex items-center justify-center text-gray text-[13px] leading-[32px]",
+                                  isSelected &&
+                                    "bg-primary text-primary-foreground",
+                                  !isSelected && "sm:hover:bg-primary/20"
+                                )}
+                              >
+                                {CALENDAR_MONTHS[month]}
+                              </button>
+                            );
+                          })}
+                      </li>
                     );
                   })}
-                </li>
-                {[...new Array(CALENDAR_WEEKS)].map((_, idx) => {
-                  return (
-                    <li
-                      key={idx}
-                      className="flex items-center justify-between text-xs"
-                    >
-                      {days
-                        .slice(
-                          idx * totalDaysInWeek,
-                          (idx + 1) * totalDaysInWeek
-                        )
-                        .map((date) => {
-                          const _date = new Date(date.join("-"));
-                          const _isToday = isToday(_date);
-                          const isCurrentMonth = selectedDate?.month
-                            ? date[1] === selectedDate?.month
-                            : date[1] === THIS_MONTH;
-
-                          let isSelectedDate = false;
-
-                          if (selectedDate?.currentDate) {
-                            const _date = new Date(selectedDate?.currentDate);
-                            const date_date = _date.getDate();
-                            const dateMonth = _date.getMonth() + 1;
-                            const dateYear = _date.getFullYear();
-                            console.log(date_date, dateMonth, dateYear);
-
-                            isSelectedDate =
-                              date_date === date[date.length - 1] &&
-                              dateMonth === date[1] &&
-                              dateYear === date[0];
-                          }
-
-                          const handleDate = () => {
+                </ul>
+              )}
+              {calendarView === "date" && (
+                <ul className="flex flex-col">
+                  <li className="calendar-weeks flex items-center justify-between text-xs">
+                    {Object.keys(WEEK_DAYS).map((day) => {
+                      return (
+                        <span
+                          key={day}
+                          className="h-7 w-7 flex items-center justify-center text-gray/40 text-center"
+                        >
+                          {WEEK_DAYS[day][0]}
+                        </span>
+                      );
+                    })}
+                  </li>
+                  {[...new Array(CALENDAR_WEEKS)].map((_, idx) => {
+                    return (
+                      <li
+                        key={idx}
+                        className="flex items-center justify-between text-xs"
+                      >
+                        {days
+                          .slice(
+                            idx * totalDaysInWeek,
+                            (idx + 1) * totalDaysInWeek
+                          )
+                          .map((date) => {
                             const _date = new Date(date.join("-"));
-                            const month = _date.getMonth() + 1;
-                            const year = _date.getFullYear();
+                            const _isToday = isToday(_date);
+                            const isCurrentMonth = selectedDate?.month
+                              ? date[1] === selectedDate?.month
+                              : date[1] === THIS_MONTH;
 
-                            setSelectedDate((prevState) => ({
-                              ...prevState,
-                              currentDate: _date,
-                              month,
-                              year,
-                            }));
-                          };
+                            let isSelectedDate = false;
 
-                          return (
-                            <button
-                              key={date.join("-")}
-                              className={cn(
-                                "h-7 w-7 flex items-center justify-center text-center rounded-full transition-colors",
-                                _isToday && "text-primary bg-primary/10",
-                                !isCurrentMonth && "text-gray/40",
-                                isSelectedDate &&
-                                  "bg-primary text-primary-foreground",
-                                !_isToday &&
-                                  !isSelectedDate &&
-                                  "sm:hover:bg-cancel-btn-hover sm:active:bg-cancel-btn-active"
-                              )}
-                              onClick={handleDate}
-                            >
-                              {date[date.length - 1]}
-                            </button>
-                          );
-                        })}
-                    </li>
-                  );
-                })}
-              </ul>
+                            if (selectedDate?.currentDate) {
+                              const _date = new Date(selectedDate?.currentDate);
+                              const date_date = _date.getDate();
+                              const dateMonth = _date.getMonth() + 1;
+                              const dateYear = _date.getFullYear();
+                              console.log(date_date, dateMonth, dateYear);
+
+                              isSelectedDate =
+                                date_date === date[date.length - 1] &&
+                                dateMonth === date[1] &&
+                                dateYear === date[0];
+                            }
+
+                            const handleDate = () => {
+                              const _date = new Date(date.join("-"));
+                              const month = _date.getMonth() + 1;
+                              const year = _date.getFullYear();
+
+                              setSelectedDate((prevState) => ({
+                                ...prevState,
+                                currentDate: _date,
+                                month,
+                                year,
+                              }));
+                            };
+
+                            return (
+                              <button
+                                key={date.join("-")}
+                                className={cn(
+                                  "h-7 w-7 flex items-center justify-center text-center rounded-full transition-colors",
+                                  _isToday && "text-primary bg-primary/10",
+                                  !isCurrentMonth && "text-gray/40",
+                                  isSelectedDate &&
+                                    "bg-primary text-primary-foreground",
+                                  !_isToday &&
+                                    !isSelectedDate &&
+                                    "sm:hover:bg-cancel-btn-hover sm:active:bg-cancel-btn-active"
+                                )}
+                                onClick={handleDate}
+                              >
+                                {date[date.length - 1]}
+                              </button>
+                            );
+                          })}
+                      </li>
+                    );
+                  })}
+                </ul>
+              )}
             </div>
           </div>
         </div>
