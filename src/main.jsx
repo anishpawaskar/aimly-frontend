6f2c1e5be9c687a4f34a9ca0b48a2b4c4cc23b55
anishpawaskar <anishpawaskar@gmail.com>
2025-06-25 15:10:36 +0530

    chore: install tanstack query and configure it along with dev tool

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,22 @@ import App from "./App.jsx";
 import { ThemeProvider } from "./context/theme-provider.jsx";
 import { BrowserRouter } from "react-router";
 import { TaskPageProvider } from "./context/task-page-provider";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
-      <ThemeProvider>
-        <TaskPageProvider>
-          <App />
-        </TaskPageProvider>
-      </ThemeProvider>
+      <QueryClientProvider client={queryClient}>
+        <ThemeProvider>
+          <TaskPageProvider>
+            <App />
+          </TaskPageProvider>
+        </ThemeProvider>
+      </QueryClientProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
     </BrowserRouter>
   </StrictMode>
 );
